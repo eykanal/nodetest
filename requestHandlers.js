f298b193e4e6de877122ca3df391a4ea10146b2c
Eliezer Kanal <ekanal@erikdev.com>
2012-12-23 21:11:49 -0500

    pass response to request handler

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,13 +1,29 @@
-function start() {
+var exec = require("child_process").exec;
+
+function start(response) {
 	console.log("Request handler 'start' was called");
+
+	exec("ls -lah", function (error, stdout, stderr) {
+		response.writeHead(200, {"Content-Type": "text/plain"});
+		response.write(stdout);
+		response.end();
+	});
 }
 
-function upload() {
+function upload(response) {
 	console.log("Request handler 'upload' was called");
+
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.write("Upload stuff.");
+	response.end();
 }
 
-function missing(handle) {
-	console.log("Request handler " + handle + " not found")
+function missing(response, handle) {
+	console.log("Request handler " + handle + " not found");
+
+	response.writeHead(404, {"Content-Type": "text/plain"});
+	response.write("Missing page");
+	response.end();
 }
 
 exports.start   = start;
--- router.js
+++ router.js
@@ -1,11 +1,12 @@
-function route(handle, pathname) {
+function route(handle, pathname, response) {
 	console.log("Routing request for " + pathname);
 
 	if( typeof handle[pathname] === 'function') {
-		handle[pathname];
+		handle[pathname](response);
 	} else {
-		handle["/404"](pathname);
+		handle["/404"](response, pathname);
 	}
+
 }
 
 exports.route = route;
